90ea31edd318b514ebc79fd89c681bd36ce290ab
조원희 <woni0517@naver.com>
2025-08-17 22:24:49 +0900

    refactor: User와 Room 매핑 설계

--- src/main/java/com/KimZo2/Back/entity/User.java
+++ src/main/java/com/KimZo2/Back/entity/User.java
@@ -1,31 +1,47 @@
 package com.KimZo2.Back.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "users")
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 @Builder
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter @Setter
 public class User {
 
     @Id
-    private String id;
+    @GeneratedValue
+    private UUID id;
 
-    private String provider; // kakao or naver or google or github
+    @Column(nullable = false)
+    private String provider;
 
-    private String providerId; // Oauth token
+    @Column(nullable = false)
+    private String providerId;
 
+    @Column(nullable = false)
     private String name;
 
-    @Indexed(unique = true)
-    private String nickname; // 필수 항목 이걸로 사용자 구분
+    @Column(nullable = false)
+    private String nickname;
 
+    @Column(nullable = false)
     private String birthday;
 
+    @Column(nullable = false)
     private boolean agreement;
+
+    @OneToMany(mappedBy = "creator", cascade = CascadeType.ALL)
+    private List<Room> rooms = new ArrayList<>();
+
+    // 연관 관계 메서드
+    public void addRooms(Room room){
+        rooms.add(room);
+        room.setUser(this);
+    }
 }
